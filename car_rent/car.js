a2e41ff36eb002b60f0069ee665ae91f1db5d618
Vrb77 <89372403+Vrb77@users.noreply.github.com>
2021-08-23 18:12:18 +0530

    Add files via upload

---
+++ car_rent/car.js
@@ -0,0 +1,29 @@
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+function getConfirmation() {
+               var retVal = confirm(" Log in successfully");
+               
+            }
+function getMsg() {
+               var retVal = confirm(" Send successfully");
+               
+            }
